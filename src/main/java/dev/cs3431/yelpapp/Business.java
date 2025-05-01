fb6c74a70563b245c64437f253217ad00fbaf85a
Xunn3714 <142214735+Xunn3714@users.noreply.github.com>
2025-05-01 19:44:54 -0400

    Update Business.java

--- src/main/java/dev/cs3431/yelpapp/Business.java
+++ src/main/java/dev/cs3431/yelpapp/Business.java
@@ -1,16 +1,25 @@
-package dev.cs3431.yelpapp;
+package querycrusher.yelpapp;
 
-public class Business {
+public class Business{
     private final String id;
     private final String name;
     private final String address;
     private final String city;
+    private final Double stars;
+    private final Double numTips;
+    private final Double latitude;
+    private final Double longitude;
 
-    public Business (String id, String name, String address, String city) {
+
+    public Business(String id, String name, String address, String city, Double stars, Double numTips, Double latitude, Double longitude) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.city = city;
+        this.stars = stars;
+        this.numTips = numTips;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getId() {
@@ -26,7 +35,19 @@ public class Business {
     }
 
     public String getCity() {
-        return city;
+    return city;
+    }
+
+    public Double getStars() {
+        return stars;
+    }
+    public Double getNumTips() {
+        return numTips;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
     }
 }
-
